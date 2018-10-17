806de1a6d327ca27c2a22d7ca8753b07f15faff5
Anshul Dubey <anshuldubey2166@gmail.com>
2018-10-17 23:20:44 +0530

    Added menu for messages and notifications

--- src/Components/Dashboard/Header/Header.js
+++ src/Components/Dashboard/Header/Header.js
@@ -1,71 +1,73 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import Badge from '@material-ui/core/Badge';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import { withStyles } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MoreIcon from '@material-ui/icons/MoreVert';
+import React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import Badge from "@material-ui/core/Badge";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import { withStyles } from "@material-ui/core/styles";
+import MenuIcon from "@material-ui/icons/Menu";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MailIcon from "@material-ui/icons/Mail";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MoreIcon from "@material-ui/icons/MoreVert";
 
-const title="Disaster Alert";
+const title = "Disaster Alert";
 
 const styles = theme => ({
   root: {
-    width: '100%',
+    width: "100%"
   },
   grow: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   menuButton: {
     marginLeft: -12,
-    marginRight: 20,
+    marginRight: 20
   },
   title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block"
+    }
   },
   inputRoot: {
-    color: 'inherit',
-    width: '100%',
+    color: "inherit",
+    width: "100%"
   },
   inputInput: {
     paddingTop: theme.spacing.unit,
     paddingRight: theme.spacing.unit,
     paddingBottom: theme.spacing.unit,
     paddingLeft: theme.spacing.unit * 10,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: 200,
-    },
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: 200
+    }
   },
   sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-    },
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex"
+    }
   },
   sectionMobile: {
-    display: 'flex',
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
-    },
-  },
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none"
+    }
+  }
 });
 
 class Header extends React.Component {
   state = {
     anchorEl: null,
     mobileMoreAnchorEl: null,
+    messageEl: null,
+    notificationEl: null
   };
 
   handleProfileMenuOpen = event => {
@@ -84,20 +86,64 @@ class Header extends React.Component {
   handleMobileMenuClose = () => {
     this.setState({ mobileMoreAnchorEl: null });
   };
-
+  handleMessageClick = event => {
+    this.setState({ messageEl: event.currentTarget });
+  };
+  handleMessageClose = () => {
+    this.setState({ messageEl: null });
+  };
+  handleNotificationClick = event => {
+    this.setState({ notificationEl: event.currentTarget });
+  };
+  handleNotificationClose = () => {
+    this.setState({ notificationEl: null });
+  };
   render() {
-    const { anchorEl, mobileMoreAnchorEl } = this.state;
+    const {
+      anchorEl,
+      mobileMoreAnchorEl,
+      messageEl,
+      notificationEl
+    } = this.state;
     const { classes } = this.props;
     const isMenuOpen = Boolean(anchorEl);
     const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-    let messages=this.props.messages;
-    let notifications=this.props.notifications;
+    const isMessageOpen = Boolean(messageEl);
+    const isNotificationOpen = Boolean(notificationEl);
+    let messages = this.props.messages;
+    let notifications = this.props.notifications;
+
+    const renderMessageMenu = (
+      <Menu
+        anchorEl={messageEl}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
+        open={isMessageOpen}
+        onClose={this.handleMessageClose}
+      >
+        <MenuItem onClick={this.handleClose}>Message 1</MenuItem>
+        <MenuItem onClick={this.handleClose}>Message 2</MenuItem>
+      </Menu>
+    );
+
+    const renderNotificationMenu = (
+      <Menu
+        anchorEl={notificationEl}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
+        open={isNotificationOpen}
+        onClose={this.handleNotificationClose}
+      >
+        <MenuItem onClick={this.handleClose}>Notification 1</MenuItem>
+        <MenuItem onClick={this.handleClose}>Notification 2</MenuItem>
+      </Menu>
+    );
 
     const renderMenu = (
       <Menu
         anchorEl={anchorEl}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
         open={isMenuOpen}
         onClose={this.handleMenuClose}
       >
@@ -109,22 +155,30 @@ class Header extends React.Component {
     const renderMobileMenu = (
       <Menu
         anchorEl={mobileMoreAnchorEl}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
         open={isMobileMenuOpen}
         onClose={this.handleMobileMenuClose}
       >
-        <MenuItem>
+        <MenuItem onClick={this.handleMessageClick}>
           <IconButton color="inherit">
-            <Badge className={classes.margin} badgeContent={messages.length} color="secondary">
+            <Badge
+              className={classes.margin}
+              badgeContent={messages.length}
+              color="secondary"
+            >
               <MailIcon />
             </Badge>
           </IconButton>
           <p>Messages</p>
         </MenuItem>
-        <MenuItem>
+        <MenuItem onClick={this.handleNotificationClick}>
           <IconButton color="inherit">
-            <Badge className={classes.margin} badgeContent={notifications.length} color="secondary">
+            <Badge
+              className={classes.margin}
+              badgeContent={notifications.length}
+              color="secondary"
+            >
               <NotificationsIcon />
             </Badge>
           </IconButton>
@@ -143,26 +197,53 @@ class Header extends React.Component {
       <div className={classes.root}>
         <AppBar position="static">
           <Toolbar>
-            <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
+            <IconButton
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Open drawer"
+            >
               <MenuIcon />
             </IconButton>
-            <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+            <Typography
+              className={classes.title}
+              variant="h6"
+              color="inherit"
+              noWrap
+            >
               {title}
             </Typography>
             <div className={classes.grow} />
             <div className={classes.sectionDesktop}>
-              <IconButton color="inherit">
-                <Badge className={classes.margin} badgeContent={messages.length} color="secondary">
+              <IconButton
+                aria-owns={isMenuOpen ? "material-appbar" : null}
+                aria-haspopup="true"
+                onClick={this.handleMessageClick}
+                color="inherit"
+              >
+                <Badge
+                  className={classes.margin}
+                  badgeContent={messages.length}
+                  color="secondary"
+                >
                   <MailIcon />
                 </Badge>
               </IconButton>
-              <IconButton color="inherit">
-                <Badge className={classes.margin} badgeContent={notifications.length} color="secondary">
+              <IconButton
+                aria-owns={isMenuOpen ? "material-appbar" : null}
+                aria-haspopup="true"
+                onClick={this.handleNotificationClick}
+                color="inherit"
+              >
+                <Badge
+                  className={classes.margin}
+                  badgeContent={notifications.length}
+                  color="secondary"
+                >
                   <NotificationsIcon />
                 </Badge>
               </IconButton>
               <IconButton
-                aria-owns={isMenuOpen ? 'material-appbar' : null}
+                aria-owns={isMenuOpen ? "material-appbar" : null}
                 aria-haspopup="true"
                 onClick={this.handleProfileMenuOpen}
                 color="inherit"
@@ -171,12 +252,18 @@ class Header extends React.Component {
               </IconButton>
             </div>
             <div className={classes.sectionMobile}>
-              <IconButton aria-haspopup="true" onClick={this.handleMobileMenuOpen} color="inherit">
+              <IconButton
+                aria-haspopup="true"
+                onClick={this.handleMobileMenuOpen}
+                color="inherit"
+              >
                 <MoreIcon />
               </IconButton>
             </div>
           </Toolbar>
         </AppBar>
+        {renderMessageMenu}
+        {renderNotificationMenu}
         {renderMenu}
         {renderMobileMenu}
       </div>
@@ -185,7 +272,7 @@ class Header extends React.Component {
 }
 
 Header.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Header);
