dd04d1c60344b6b48497edbac981359cff4e9765
Anshul Dubey <anshuldubey2166@gmail.com>
2018-10-17 21:01:15 +0530

    Added extra responsive support for feed styling

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -3,9 +3,11 @@ import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import './Feed.css';
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import "./Feed.css";
 
 const NewsFeedCard = data => {
   return (
@@ -21,13 +23,13 @@ const NewsFeedCard = data => {
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
+      <CardActions className="card-actions-row">
+        <IconButton aria-label="Add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="Share">
+          <ShareIcon />
+        </IconButton>
       </CardActions>
     </Card>
   );
