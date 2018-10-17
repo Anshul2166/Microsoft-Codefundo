14c7ca27ace13296ac90e1bfcc2e30ded0873f82
Anshul Dubey <anshuldubey2166@gmail.com>
2018-10-17 19:08:09 +0530

    Adding marker from current position

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -9,26 +9,65 @@ import "./Map.css";
 
 const MapWithAMarker = withScriptjs(
   withGoogleMap(props => (
-    <GoogleMap defaultZoom={15} defaultCenter={{ lat: props.currentPosition.lat, lng: props.currentPosition.lng }}>
-      <Marker position={{ lat: props.currentPosition.lat, lng: props.currentPosition.lng }} />
+    <GoogleMap
+      defaultZoom={8}
+      defaultCenter={{
+        lat: props.currentPosition.lat,
+        lng: props.currentPosition.lng
+      }}
+    >
+      <Marker
+        position={{
+          lat: props.currentPosition.lat,
+          lng: props.currentPosition.lng
+        }}
+      />
     </GoogleMap>
   ))
 );
 
 class Map extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       //Set Raipur's location as default location
-      currentPosition:{
-        lat:21.2514,
-        lng:81.6296,
+      currentPosition: {
+        lat: 51.2514,
+        lng: 81.6296
       }
     };
   }
+  componentDidMount(){
+    this.getGeoLocation();
+  }
+  getGeoLocation = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                console.log(position.coords);
+                this.setState(prevState => ({
+                  currentPosition: {
+                    ...prevState.currentPosition,
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                  },
+                  isMarkerShown: true
+                }));
+                console.log(this.state);
+            }
+        )
+    } else {
+        alert("Location access denied");
+    }
+  }
+
   render() {
-    const title=process.env.REACT_APP_GOOGLE_MAPS_API;
-    let url="https://maps.googleapis.com/maps/api/js?key="+title+"&v=3.exp&libraries=geometry,drawing,places";
+    const title = process.env.REACT_APP_GOOGLE_MAPS_API;
+    let url =
+      "https://maps.googleapis.com/maps/api/js?key=" +
+      title +
+      "&v=3.exp&libraries=geometry,drawing,places";
+    // this.getGeoLocation();
     return (
       <MapWithAMarker
         currentPosition={this.state.currentPosition}
