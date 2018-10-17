26064c55ef8477cb430c881ee737623406a8d82a
Anshul Dubey <anshuldubey2166@gmail.com>
2018-10-17 18:37:29 +0530

    Getting map coordinates from props

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -9,18 +9,29 @@ import "./Map.css";
 
 const MapWithAMarker = withScriptjs(
   withGoogleMap(props => (
-    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-      <Marker position={{ lat: -34.397, lng: 150.644 }} />
+    <GoogleMap defaultZoom={15} defaultCenter={{ lat: props.currentPosition.lat, lng: props.currentPosition.lng }}>
+      <Marker position={{ lat: props.currentPosition.lat, lng: props.currentPosition.lng }} />
     </GoogleMap>
   ))
 );
 
 class Map extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      //Set Raipur's location as default location
+      currentPosition:{
+        lat:21.2514,
+        lng:81.6296,
+      }
+    };
+  }
   render() {
     const title=process.env.REACT_APP_GOOGLE_MAPS_API;
     let url="https://maps.googleapis.com/maps/api/js?key="+title+"&v=3.exp&libraries=geometry,drawing,places";
     return (
       <MapWithAMarker
+        currentPosition={this.state.currentPosition}
         googleMapURL={url}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `400px` }} />}
