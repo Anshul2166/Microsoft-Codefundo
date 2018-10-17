817e1cc0ce28c4f1aa2de10660cbd68f3f50e310
Anshul Dubey <anshuldubey2166@gmail.com>
2018-10-17 18:29:15 +0530

    Static map working

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
 import "./Map.css";
 
-const Map = () => (
-  <p>Hello</p>
+const MapWithAMarker = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+      <Marker position={{ lat: -34.397, lng: 150.644 }} />
+    </GoogleMap>
+  ))
 );
+
+class Map extends Component {
+  render() {
+    const title=process.env.REACT_APP_GOOGLE_MAPS_API;
+    let url="https://maps.googleapis.com/maps/api/js?key="+title+"&v=3.exp&libraries=geometry,drawing,places";
+    return (
+      <MapWithAMarker
+        googleMapURL={url}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `400px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    );
+  }
+}
 
 export default Map;
