c6a8779921e9f65978a4aa7a9d3c06bfbf7f3e5f
Anshul Dubey <anshuldubey2166@gmail.com>
2018-10-19 09:57:46 +0530

    Added and styled details card

--- src/Components/Common/Details/Details.js
+++ src/Components/Common/Details/Details.js
@@ -1,21 +1,33 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
-import red from '@material-ui/core/colors/red';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import blue from "@material-ui/core/colors/blue";
 import Grid from "@material-ui/core/Grid";
+import logo from "../../../Common/Images/pressure.png";
+import AlarmIcon from "@material-ui/icons/Alarm";
+
 const styles = theme => ({
   card: {
     maxWidth: 600,
-    margin:15
+    margin: 15,
+    padding:10
   },
   avatar: {
-    backgroundColor: red[500],
+    backgroundColor: blue[500]
   },
+  iconSmall: {
+    marginLeft: 15,
+    marginRight: 15
+  },
+  verticalCenter:{
+    marginTop:"auto", 
+    marginBottom:"auto"
+  }
 });
 
 class Details extends React.Component {
@@ -24,30 +36,30 @@ class Details extends React.Component {
 
     return (
       <Grid item sm={3} xs={6}>
-      <Card className={classes.card}>
-        <CardHeader
-          avatar={
-            <Avatar aria-label="Recipe" className={classes.avatar}>
-              R
-            </Avatar>
-          } 
-          title="Shrimp and Chorizo Paella"
-          subheader="September 14, 2016"
-        />
-        <CardContent>
-          <Typography variant="caption">
-            This impressive paella is a perfect party dish and a fun meal to cook together with your
-            guests. Add 1 cup of frozen peas along with the mussels, if you like.
-          </Typography>
-        </CardContent>
-      </Card>
+        <Card className={classes.card}>
+          <CardHeader
+            avatar={
+              <Avatar aria-label="Recipe" className={classes.avatar}>
+                R
+              </Avatar>
+            }
+            title="Tornado Pressure"
+            subheader="100 atm"
+          />
+          <CardContent>
+            <Grid container spacing={24}>
+              <AlarmIcon className={classes.iconSmall}/>
+              <Typography variant="caption" className={classes.verticalCenter}>Last updated 2 hours ago</Typography>
+            </Grid>
+          </CardContent>
+        </Card>
       </Grid>
     );
   }
 }
 
 Details.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Details);
